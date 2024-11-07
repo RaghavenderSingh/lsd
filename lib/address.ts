8b707f81a16366c5ce6c1f9bf31d7c1a819cfd01
Raghavender Singh <itsraghavchouhan23@gmail.com>
2024-11-07 18:04:10 +0530

    fix

--- lib/address.ts
+++ lib/address.ts
@@ -1,9 +1,6 @@
+
+
 export const PUBLIC_KEY = process.env.PUBLIC_KEY!;
 export const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 export const TOKEN_MINT_ADDRESS = process.env.TOKEN_MINT_ADDRESS!;
 
-export function convertToTokenDecimalUnit(amount: number){
-    const res = amount*LAMPORTS_PER_SOL;
-    console.log(res);
-    return res;
-}
